cbe1eb975237767d4f915811c2193998d74d0563
qianlixin <1342294200@qq.com>
2020-10-09 17:44:10 +0800

    19天

---
+++ SwiftDemo/Day20/BaseAnimations.swift
@@ -0,0 +1,30 @@
+//
+//  BaseAnimations.swift
+//  SwiftDemo
+//
+//  Created by 钱立新 on 2020/10/9.
+//  Copyright © 2020 钱立新. All rights reserved.
+//
+
+import UIKit
+
+class BaseAnimations: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
